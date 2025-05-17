9092e2e39349aeb9650532b2b9a444ce9c2f672d
Awowz <tonygriffith75@gmail.com>
2025-05-16 20:02:11 -0500

    changing direction and will be implementing sqllite for inventory

--- inventory_lsit.py
+++ inventory_lsit.py
@@ -1,12 +1,22 @@
 from item import *
+import sqlite3
+from constants import *
 
 class Inventory_List:
     def __init__(self):
-        self.inventory = {}
+        self.sql_connection = sqlite3.connect(SQL_FILE_NAME)
+        self.sql_inventory = self.sql_connection.cursor()
 
-    def add_item(self, item:Item):
-        item_id = item.get_item_str_index()
-        if item_id in self.inventory:
-            self.inventory[item_id].append(item)
-        else:
-            self.inventory[item_id] = [item]
+    def create_table(self):
+        query = f"SELECT name FROM sqlite_master WHERE type='table' and name='{SQL_INVENTORY_TABLE_NAME}';"
+        listOfTables = self.sql_inventory.execute(query).fetchall()
+        if listOfTables != []:
+            return
+        #query = f"CREATE TABLE {SQL_INVENTORY_TABLE_NAME}({SQL_INVENTORY_INDEX} int, );"
+        
+
+ 
+        
+
+    def import_from_google_sheets(self):
+        pass
